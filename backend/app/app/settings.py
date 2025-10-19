f4a238bfaead097f14ba7ab063eb447768435bff
nishanthkj <nishanthkj12@gmail.com>
2025-10-19 21:10:41 +0530

    CORS

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -56,7 +56,7 @@ cors_origins = os.getenv("CORS_ALLOWED_ORIGINS", "")
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://codingal-ai-course-coach-fullstack-40ydtjtny.vercel.app"
+    "https://codingal-ai-course-coach-fullstack.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
