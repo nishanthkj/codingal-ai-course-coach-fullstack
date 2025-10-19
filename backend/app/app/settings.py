6733ee596d3998cb8d6bc00758035b2a791cff70
nishanthkj <nishanthkj12@gmail.com>
2025-10-19 20:16:20 +0530

    CORS

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -51,9 +51,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+cors_origins = os.getenv("CORS_ALLOWED_ORIGINS", "")
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://codingal-ai-course-coach-fullstack-40ydtjtny.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
